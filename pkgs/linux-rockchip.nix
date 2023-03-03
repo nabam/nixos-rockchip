91f8c3d69b7aae22464d92704ae8111d2eae9c33
Leo Popov <leo@nabam.net>
2023-03-03 15:21:47 +0100

    add SND_SOC_ROCKCHIP_I2S_TDM to kernel

--- pkgs/linux-rockchip.nix
+++ pkgs/linux-rockchip.nix
@@ -2,38 +2,39 @@
 
 let
   kernelConfig = with lib.kernel; {
-    SND_SOC_RK817 = yes;
-    STMMAC_ETH = yes;
-    MOTORCOMM_PHY = yes;
+    ARCH_ROCKCHIP = yes;
+    CHARGER_RK817 = yes;
+    COMMON_CLK_RK808 = yes;
+    COMMON_CLK_ROCKCHIP = yes;
+    GPIO_ROCKCHIP = yes;
+    MFD_RK808 = yes;
     MMC_DW = yes;
     MMC_DW_ROCKCHIP = yes;
     MMC_SDHCI_OF_DWCMSHC = yes;
+    MOTORCOMM_PHY = yes;
     PCIE_ROCKCHIP_DW_HOST = yes;
-    PHY_ROCKCHIP_NANENG_COMBO_PHY = yes;
-    ROCKCHIP_DW_HDMI = yes;
+    PHY_ROCKCHIP_INNO_CSIDPHY = yes;
     PHY_ROCKCHIP_INNO_DSIDPHY = yes;
-    ROCKCHIP_VOP2 = yes;
-    ARCH_ROCKCHIP = yes;
-    ROCKCHIP_PHY = yes;
     PHY_ROCKCHIP_INNO_USB2 = yes;
-    RTC_DRV_RK808 = yes;
-    COMMON_CLK_RK808 = yes;
-    MFD_RK808 = yes;
-    CHARGER_RK817 = yes;
-    REGULATOR_RK808 = yes;
-    ROCKCHIP_PM_DOMAINS = yes;
-    GPIO_ROCKCHIP = yes;
+    PHY_ROCKCHIP_NANENG_COMBO_PHY = yes;
     PINCTRL_ROCKCHIP = yes;
     PWM_ROCKCHIP = yes;
+    REGULATOR_RK808 = yes;
+    ROCKCHIP_DW_HDMI = yes;
+    ROCKCHIP_IODOMAIN = yes;
     ROCKCHIP_IOMMU = yes;
     ROCKCHIP_MBOX = yes;
+    ROCKCHIP_PHY = yes;
+    ROCKCHIP_PM_DOMAINS = yes;
     ROCKCHIP_SARADC = yes;
     ROCKCHIP_THERMAL = yes;
+    ROCKCHIP_VOP2 = yes;
+    RTC_DRV_RK808 = yes;
+    SND_SOC_RK817 = yes;
+    SND_SOC_ROCKCHIP_I2S_TDM = yes;
     SPI_ROCKCHIP = yes;
+    STMMAC_ETH = yes;
     VIDEO_HANTRO_ROCKCHIP = yes;
-    ROCKCHIP_IODOMAIN = yes;
-    COMMON_CLK_ROCKCHIP = yes;
-    PHY_ROCKCHIP_INNO_CSIDPHY = yes;
   };
 in
 with pkgs.linuxKernel;

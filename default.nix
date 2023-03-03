7699d4a7828c6f0620f966d9590792c1f165e8f7
Leo Popov <leo@nabam.net>
2023-03-03 18:50:21 +0100

    update default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,22 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  buildImage = uBootPkg: (
+  buildImage = { kernel, uBoot }: (
     with (import (<nixpkgs/nixos/lib/eval-config.nix>) {
       modules = [
           ./modules/sd-card/sd-image-rockchip-installer.nix
-          { rockchip.uBoot = uBootPkg;  }
+          { rockchip.uBoot = uBoot;  }
       ];
     }); {
       inherit (config.system.build) sdImage;
     }
   );
-  uBoot = pkgs.callPackage ./pkgs/uboot-rockchip.nix {};
+  uBoot  = pkgs.callPackage ./pkgs/uboot-rockchip.nix {};
+  kernel = pkgs.callPackage ./pkgs/linux-rockchip.nix {};
 in {
-  quartz64a = ( buildImage uBoot.uBootQuartz64A ).sdImage;
-  quartz64b = ( buildImage uBoot.uBootQuartz64B ).sdImage;
-  soQuartz  = ( buildImage uBoot.uBootSoQuartz  ).sdImage;
+  quartz64a      = ( buildImage { uBoot = uBoot.uBootQuartz64A;      kernel = kernel.linux_6_1_rockchip; } ).sdImage;
+  quartz64b      = ( buildImage { uBoot = uBoot.uBootQuartz64B;      kernel = kernel.linux_6_1_rockchip; } ).sdImage;
+  soQuartzModelA = ( buildImage { uBoot = uBoot.uBootSoQuartzModelA; kernel = kernel.linux_6_2_rockchip; } ).sdImage;
+  soQuartzCM4IO  = ( buildImage { uBoot = uBoot.uBootSoQuartzCM4IO;  kernel = kernel.linux_6_2_rockchip; } ).sdImage;
+  soQuartzBlade  = ( buildImage { uBoot = uBoot.uBootSoQuartzBlade;  kernel = kernel.linux_6_2_rockchip; } ).sdImage;
 }

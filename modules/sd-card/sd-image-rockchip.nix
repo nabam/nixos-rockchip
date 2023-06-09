dcc30141bae481416a18d7268239b7b36ffa5918
Lev Popov <leo@nabam.net>
2023-06-09 19:11:29 +0200

    remove module genericExtlinuxCompatiblePatched

--- modules/sd-card/sd-image-rockchip.nix
+++ modules/sd-card/sd-image-rockchip.nix
@@ -3,6 +3,7 @@
   imports = [
     (modulesPath + "/profiles/base.nix")
     (modulesPath + "/installer/sd-card/sd-image.nix")
+    (modulesPath + "/generic-extlinux-compatible")
   ];
 
   options = {

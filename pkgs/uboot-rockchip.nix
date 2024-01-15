9b893a44e44a0dc5cffd874f39a8c4d4ecec2949
asonix <asonix@asonix.dog>
2024-01-15 16:24:45 -0500

    Update kernel to 6.6, NixOS to 23.11, add patches for rk3399 and rk3328 uboots (#16)
    * Update kernel to 6.6, update nixpkgs to 23.11
    
    Build uBoots for non-quartz devices
    
    * Add additional uBoots to github actions
    
    ---------
    
    Co-authored-by: Lev Popov <leo@nabam.net>

--- pkgs/uboot-rockchip.nix
+++ pkgs/uboot-rockchip.nix
@@ -1,14 +1,8 @@
 { pkgs, stdenv, lib, fetchpatch, fetchFromGitHub, buildUBoot, buildPackages }:
 
 let 
-  buildRK3566UBoot = defconfig: let
-    inherit defconfig;
-    rkbin = fetchFromGitHub {
-      owner = "rockchip-linux";
-      repo = "rkbin";
-      rev = "b4558da0860ca48bf1a571dd33ccba580b9abe23";
-      sha256 = "KUZQaQ+IZ0OynawlYGW99QGAOmOrGt2CZidI3NTxFw8=";
-    };
+  buildPatchedUBoot = { defconfig, BL31, ROCKCHIP_TPL ? "" }: let
+    inherit defconfig BL31 ROCKCHIP_TPL;
     src = fetchFromGitHub {
       owner = "u-boot";
       repo = "u-boot";
@@ -28,6 +22,7 @@ let
         url = "https://github.com/Kwiboo/u-boot-rockchip/compare/4459ed60cb1e0562bc5b40405e2b4b9bbf766d57...0bc339ffa6f804d51c5c5292d8ff69c4d79614d3.diff";
         sha256 = "ui77Nm3IS6PUzaMagqyyZDitklot8MmeYg27mVPV7Pc=";
       })
+      ./ramdisk_addr_r.patch
     ];
 
     buildInputs = with buildPackages; [
@@ -57,13 +52,34 @@ let
       "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
     ];
 
-    BL31 = (rkbin + "/bin/rk35/rk3568_bl31_v1.43.elf");
-    ROCKCHIP_TPL = (rkbin + "/bin/rk35/rk3566_ddr_1056MHz_v1.18.bin");
+    BL31 = BL31;
+    ROCKCHIP_TPL = ROCKCHIP_TPL;
 
     extraMeta = {
       platforms = [ "aarch64-linux" ];
       license = lib.licenses.unfreeRedistributableFirmware;
     };
+  };
+  buildRk3328UBoot = defconfig: buildPatchedUBoot {
+    inherit defconfig;
+    BL31 = "${pkgs.armTrustedFirmwareRK3328}/bl31.elf";
+  };
+  buildRK3399UBoot = defconfig: buildPatchedUBoot {
+    inherit defconfig;
+    BL31 = "${pkgs.armTrustedFirmwareRK3399}/bl31.elf";
+  };
+  buildRK3566UBoot = defconfig: let
+    rkbin = fetchFromGitHub {
+      owner = "rockchip-linux";
+      repo = "rkbin";
+      rev = "b4558da0860ca48bf1a571dd33ccba580b9abe23";
+      sha256 = "KUZQaQ+IZ0OynawlYGW99QGAOmOrGt2CZidI3NTxFw8=";
+    };
+  in
+  buildPatchedUBoot { 
+    inherit defconfig; 
+    BL31 = (rkbin + "/bin/rk35/rk3568_bl31_v1.43.elf");
+    ROCKCHIP_TPL = (rkbin + "/bin/rk35/rk3566_ddr_1056MHz_v1.18.bin");
   };
   buildRK3566UBoot_2022_04 = defconfig: let
     inherit defconfig;
@@ -112,6 +128,10 @@ in {
   uBootSoQuartzCM4IO  = buildRK3566UBoot "soquartz-cm4-rk3566_defconfig";
   uBootSoQuartzModelA = buildRK3566UBoot "soquartz-model-a-rk3566_defconfig";
   uBootPineTab2       = buildRK3566UBoot "pinetab2-rk3566_defconfig";
+  uBootPinebookPro    = buildRK3399UBoot "pinebook-pro-rk3399_defconfig";
+  uBootRockPro64      = buildRK3399UBoot "rockpro64-rk3399_defconfig";
+  uBootROCPCRK3399    = buildRK3399UBoot "roc-pc-rk3399_defconfig";
+  uBootRock64         = buildRk3328UBoot "rock64-rk3328_defconfig";
 
   uBootQuartz64A_2022_04 = buildRK3566UBoot_2022_04 "quartz64-a-rk3566_defconfig";
   uBootQuartz64B_2022_04 = buildRK3566UBoot_2022_04 "quartz64-b-rk3566_defconfig";

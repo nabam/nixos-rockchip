c0fb1384a496f174f157f2fe218d8321fa4060f3
Lev Popov <leo@nabam.net>
2024-01-29 09:12:24 +0100

    remove old uboot

--- pkgs/uboot-rockchip.nix
+++ pkgs/uboot-rockchip.nix
@@ -81,46 +81,6 @@ let
     BL31 = (rkbin + "/bin/rk35/rk3568_bl31_v1.43.elf");
     ROCKCHIP_TPL = (rkbin + "/bin/rk35/rk3566_ddr_1056MHz_v1.18.bin");
   };
-  buildRK3566UBoot_2022_04 = defconfig: let
-    inherit defconfig;
-    rkbin = fetchFromGitHub {
-      owner = "rockchip-linux";
-      repo = "rkbin";
-      rev = "b0c100f1a260d807df450019774993c761beb79d";
-      sha256 = "V7RcQj3BgB2q6Lgw5RfcPlOTZF8dbC9beZBUsTvaky0=";
-    };
-    src = fetchFromGitHub {
-      owner = "u-boot";
-      repo = "u-boot";
-      rev = "v2022.04-rc1";
-      sha256 = "grFRNvr/Z5tQxqj/16q3JpD/ncLozdqSKfYZqa1SGzg=";
-    };
-    version = "v2022.04-rc1-quartz64-2ea8c1c";
-  in buildUBoot {
-    src = src;
-    version = version;
-    defconfig = defconfig;
-    extraMakeFlags = [ "all" "u-boot.itb" ];
-    filesToInstall = [ "u-boot.itb" "idbloader.img"];
-
-    patches = [
-      (fetchpatch {
-        name = "quartz64.patch";
-        url = "https://github.com/CounterPillow/u-boot-quartz64/compare/df887a045a1d726bbd654ef266e5cbe8cc0c2db3...2ea8c1c4db6d739273ae3821970c02e9d3b6180b.diff";
-        sha256 = "tA0wuallJVoA585zpGWywQco/iBKQMBBKH4ReXme8Uc=";
-      })
-    ];
-    
-    preConfigure = ''
-        ln -sf ${rkbin}/bin/rk35/rk3568_bl31_v1.34.elf ./bl31.elf
-        ln -sf ${rkbin}/bin/rk35/rk3566_ddr_1056MHz_v1.13.bin ./ram_init.bin
-    '';
-
-    extraMeta = {
-      platforms = [ "aarch64-linux" ];
-      license = lib.licenses.unfreeRedistributableFirmware;
-    };
-  };
 in {
   uBootQuartz64A      = buildRK3566UBoot "quartz64-a-rk3566_defconfig";
   uBootQuartz64B      = buildRK3566UBoot "quartz64-b-rk3566_defconfig";
@@ -132,8 +92,4 @@ in {
   uBootRockPro64      = buildRK3399UBoot "rockpro64-rk3399_defconfig";
   uBootROCPCRK3399    = buildRK3399UBoot "roc-pc-rk3399_defconfig";
   uBootRock64         = buildRk3328UBoot "rock64-rk3328_defconfig";
-
-  uBootQuartz64A_2022_04 = buildRK3566UBoot_2022_04 "quartz64-a-rk3566_defconfig";
-  uBootQuartz64B_2022_04 = buildRK3566UBoot_2022_04 "quartz64-b-rk3566_defconfig";
-  uBootSoQuartz_2022_04  = buildRK3566UBoot_2022_04 "soquartz-rk3566_defconfig";
 }

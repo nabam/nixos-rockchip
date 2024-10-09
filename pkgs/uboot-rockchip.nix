67f754e93aee351cf25841e9baf41d4a68c5891b
Sean Gilligan <sean@msgilligan.com>
2024-10-09 13:23:25 -0700

    Add RadxaRock4 (old name Rock Pi 4)
    There is no distinguishing between the various models (I don't know if
    there needs to be) but I have tested on a Rock Pi 4 SE, but when I query
    U-Boot for board info it lists a 4a.

--- pkgs/uboot-rockchip.nix
+++ pkgs/uboot-rockchip.nix
@@ -71,4 +71,5 @@ in {
   uBootROCPCRK3399 = buildRK3399UBoot "roc-pc-rk3399_defconfig";
   uBootRock64 = buildRK3328UBoot "rock64-rk3328_defconfig";
   uBootOrangePiCM4 = buildRK3566UBoot "orangepi-3b-rk3566_defconfig";
+  uBootRadxaRock4 = buildRK3399UBoot "rock-pi-4-rk3399_defconfig";
 }

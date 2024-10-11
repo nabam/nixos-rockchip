58de19628292c5a503e2822a64f93c5c23301ea3
Sean Gilligan <sean@msgilligan.com>
2024-10-11 08:13:08 -0700

    Add RadxaCM3IO

--- pkgs/uboot-rockchip.nix
+++ pkgs/uboot-rockchip.nix
@@ -71,4 +71,5 @@ in {
   uBootROCPCRK3399 = buildRK3399UBoot "roc-pc-rk3399_defconfig";
   uBootRock64 = buildRK3328UBoot "rock64-rk3328_defconfig";
   uBootOrangePiCM4 = buildRK3566UBoot "orangepi-3b-rk3566_defconfig";
+  uBootRadxaCM3IO = buildRK3566UBoot "radxa-cm3-io-rk3566_defconfig";
 }
